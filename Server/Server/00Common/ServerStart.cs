71c266059dfc24763e3991dbff94d4209591ac94
hecaiyi1997 <1332897386@qq.com>
2023-12-17 22:45:44 +0800

    ok

---
+++ Server/Server/00Common/ServerStart.cs
@@ -0,0 +1,31 @@
+﻿/****************************************************
+	文件：ServerStart.cs
+
+
+	
+	功能：服务器入口
+*****************************************************/
+
+using System;
+using System.IO;
+using System.Threading;
+
+class ServerStart
+{
+    private static Thread m_MsgThread;
+
+    static void Main(string[] args)
+    {
+        //m_MsgThread = new Thread(MsgThread);
+        //m_MsgThread.IsBackground = true;
+        //m_MsgThread.Start();
+
+        ServerRoot.Instance.Init();
+        while (true)
+        {
+            ServerRoot.Instance.Update();
+            Thread.Sleep(20);
+        }
+        //Debug.LogError("jjj");
+    }
+}
---
+++ Server/Server/NetSvc.cs
@@ -0,0 +1,22 @@
+﻿
+
+/// <summary>
+/// 网络服务
+/// </summary>
+class NetSvc
+{
+    private static NetSvc instance = null;
+    public static NetSvc Instance
+    {
+        get
+        {
+            if (instance == null) instance = new NetSvc();
+            return instance;
+        }
+    }
+
+    public void Init()
+    {
+        
+    }
+}
